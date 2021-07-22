76265b5dbd3a3fad5dc18fc40afc9c3844d78829
hydok <hydok@naver.com>
2021-07-23 01:43:46 +0900

    create model , service.dart

--- flutter_mvvm_tutorial/lib/main.dart
+++ flutter_mvvm_tutorial/lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter222 Demo Home Page'),
     );
   }
 }
